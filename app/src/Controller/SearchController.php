bf5f716f56f8267919c4a46b077d25b9dfa53a83
kinga2468 <kinga.bozecka@op.pl>
2019-06-19 17:53:10 +0200

    19.06 - 17.53
    nie jest żle, coś wyszukuje ale trochę błędów

--- app/src/Controller/SearchController.php
+++ app/src/Controller/SearchController.php
@@ -27,22 +27,82 @@ class SearchController extends AbstractController
      *     name="search_index",
      * )
      */
-    public function index(Request $request): Response
+    public function index(Request $request, IngredientRepository $ingredientRepository): Response
     {
         $search = $request->get('search');
+        $searches = explode(', ', $search);
+        $uniqueRecipes = [];
+//        dump($searches);
         if($search){
-            if(preg_match('/^\w+[\w]*$/', $search)){
-                return $this->redirectToRoute('search_action', ['search'=>$search]);
+            foreach($searches as $ingredients){
+//                dump($ingredients);
+                $ingredientResult = $ingredientRepository->findRecipeByIngredient($ingredients)->getQuery()->getResult();
+//                dump($ingredientResult);
+                foreach ($ingredientResult as $ingredient){
+                    $uniqueRecipe = [];
+                    foreach ($ingredient->getRecipes() as $recipe) {
+                        if(!in_array($recipe, $uniqueRecipe)){
+                            array_push($uniqueRecipe, $recipe);
+                        }
+                    }
+                    array_push($uniqueRecipes, $uniqueRecipe);
+                }
             }
-            else{
-                $this->addFlash('warning','message.search_error');
-            }
+//            dump($uniqueRecipes);
+        }
+        else{
+            $this->addFlash('warning','message.search_error');
+            $ingredientResult=[];
+            $uniqueRecipe=[];
         }
 
+
+
+
+//        if($search){
+//            if(preg_match('/^\w+[\w]*$/', $search)){
+//                return $this->redirectToRoute('search_action', ['search'=>$search]);
+//            }
+//            else{
+//                $this->addFlash('warning','message.search_error');
+//            }
+//        }
+
         return $this->render(
-            'search/index.html.twig'
+            'search/view.html.twig',
+            [
+                'search' => $search,
+                'ingredients' =>$ingredientResult,
+                'recipes' => $uniqueRecipe
+            ]
         );
     }
+//    public function index(Request $request) {
+//        if ($request->getMethod() == 'GET') {
+//            $title = $request->get('search');
+//            $em = $this->getDoctrine()->getManager();
+//            $qb = $em->getRepository('YCRYcrBundle:Ingredient')
+//                ->createQueryBuilder('i');
+//            $searches= explode(' ', $title);
+//
+//            foreach ($searches as $sk => $sv) {
+//                $cqb[]=$qb->expr()->like("CONCAT($sv, '')", "'%$sv%'");
+//            }
+//
+//            $qb->andWhere(call_user_func_array(array($qb->expr(),"orx"),$cqb));
+//
+//            $recipes = $qb->getResult();
+//        }else{
+//            $recipes=[];
+//        }
+//        dump($recipes);
+//        return $this->render(
+//            'search/index.html.twig',
+//            [
+//                'recipes' => $recipes
+//            ]
+//        );
+//    }
 
     /**
      * View action.
@@ -59,6 +119,8 @@ class SearchController extends AbstractController
      */
     public function view(string $search, IngredientRepository $ingredientRepository): Response
     {
+//        dump($search);
+//        die();
         $ingredientResult = $ingredientRepository->findRecipeByIngredient($search)->getQuery()->getResult();
         $uniqueRecipe = [];
 //        dump($ingredientResult);
